89eb381f6e2b2119eaf9694feb2f5fc1a0fa60fb
nathan hains <nathanhains14@yahoo.com>
2021-05-19 13:25:17 -0400

    commit tree 8d75c6945b9624fda051ecd7afb11abcffe9b5d6

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+class Cat {
+    constructor (name){
+        this.name = name
+    }
+
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor (name) {
+        this.name = name
+    }
+
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor (name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak(){
+        let s = this.sex === "female" ? `${this.name} says squawk!` : `It\'s me! ${this.name}, the parrot!`
+        return s
+    }
+}
